916535f7f5417c4ba430587a445d84d72ec7e1e2
Tanvir Alam <ta@casalogic.dk>
2016-12-27 23:39:14 +0100

    Same test case for price
    - Did it Jeffery way
    - set it through constructor
    - tested it accordingly

--- tests/unit/ProductTest.php
+++ tests/unit/ProductTest.php
@@ -6,16 +6,14 @@ class ProductTest extends PHPUnit_Framework_TestCase
 {
     function testAProductHasName()
     {
-        $product = new Product('Fallout 4');
+        $product = new Product('Fallout 4', 400);
 
         $this->assertEquals('Fallout 4', $product->name());
     }
 
     function testAProductPrice()
     {
-        $product = new Product('Fallout 4');
-
-        $product->setPrice(400);
+        $product = new Product('Fallout 4', 400);
 
         $this->assertEquals(400, $product->price());
 
