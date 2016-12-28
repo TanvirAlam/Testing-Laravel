8bfec959caa4ccbc97107ecbbf3b44351e32333d
Tanvir Alam <ta@casalogic.dk>
2016-12-28 12:39:33 +0100

    Created an Order model to test the Total Number Of products

---
+++ tests/unit/OrderTest.php
@@ -0,0 +1,21 @@
+<?php
+
+use App\Product;
+use App\Order;
+
+class OrderTest extends PHPUnit_Framework_TestCase
+{
+    /** @test */
+    function anOrderConsistsOfProducts()
+    {
+        $order = new Order;
+
+        $product1 = new Product('Fallout 4', 59);
+        $product2 = new Product('Fallout 9', 7);
+
+        $order->add($product1);
+        $order->add($product2);
+
+        $this->assertEquals(2, count($order->products()));
+    }
+}
