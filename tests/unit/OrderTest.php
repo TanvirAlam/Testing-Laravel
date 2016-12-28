9e4140d88ccbff5126aa462818bcd52016d16926
Tanvir Alam <ta@casalogic.dk>
2016-12-28 13:08:05 +0100

    Refactor Order test to match DRY and cleaning

--- tests/unit/OrderTest.php
+++ tests/unit/OrderTest.php
@@ -8,19 +8,20 @@ class OrderTest extends PHPUnit_Framework_TestCase
     /** @test */
     function anOrderConsistsOfProducts()
     {
-        $order = new Order;
-
-        $product1 = new Product('Fallout 4', 59);
-        $product2 = new Product('Fallout 9', 7);
-
-        $order->add($product1);
-        $order->add($product2);
+        $order = $this->createOrderWithProducts();
 
         $this->assertCount(2, $order->products());
     }
 
     /** @test  */
     function anOrderCanDetermineTheTotalCostOfAllItsProducts()
+    {
+        $order = $this->createOrderWithProducts();
+
+        $this->assertEquals(66, $order->total());
+    }
+
+    protected function createOrderWithProducts()
     {
         $order = new Order;
 
@@ -30,6 +31,6 @@ class OrderTest extends PHPUnit_Framework_TestCase
         $order->add($product1);
         $order->add($product2);
 
-        $this->assertEquals(66, $order->total());
+        return $order;
     }
 }
