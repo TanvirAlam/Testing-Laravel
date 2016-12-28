4650d6924a02f1249bf8427bc88d924132b8414d
Tanvir Alam <ta@casalogic.dk>
2016-12-28 13:02:21 +0100

    Refactor total method by calling array_reduce

--- app/Order.php
+++ app/Order.php
@@ -17,13 +17,8 @@ class Order
 
     public function total()
     {
-        $total = 0;
-
-        foreach ($this->products as $product)
-        {
-            $total += $product->price();
-        }
-
-        return $total;
+        return array_reduce($this->products, function($carry, $product) {
+            return $carry + $product->price();
+        });
     }
 }
